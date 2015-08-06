80e0736e507f569160b7b7cb2b1f6bfd2bd5db77
Elijah <littlerivernerd@gmail.com>
2015-08-05 18:51:04 -0700

    Revert "Revert "Lots of changes (sorry!) including: 	Major aditions to AiController. Including fireing, aiming, etc. 	Modifications to Ship in order to allow AiController to better 	functin including: 		Centered the ship shape 		Added methods to get average projectile speed 	Because of ship centering other things including drawing ships, 	cannons, and slots had to be modified. 	Ship generator was also modified to be more usefull, and player 	ship now is created with ship generator.""
    This reverts commit 046ce9f11e1485fbe18c261d3657daef43c2fadb.

---
+++ core/src/com/pirates/game/Slot.java
@@ -0,0 +1,57 @@
+package com.pirates.game;
+
+import java.util.ArrayList;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Slot {
+	Vector2 offset;
+	int size;
+	float dir;//in rad, ranging from 0 to 2pi
+	FireingDirection side;
+	Equipment inslot;
+	Ship owner;
+	/**
+	 * 
+	 * @param xOffset
+	 * @param yOffset
+	 * @param sizeOfSlot not yet implemented
+	 * @param slotDir angle slot fires, in degrees
+	 * @param fireSlot the key which fires this slot
+	 */
+	Slot(float xOffset, float yOffset, int sizeOfSlot,float slotDir, FireingDirection fireSlot,Ship ownedBy) {
+		offset = new Vector2(xOffset,yOffset);
+		size = sizeOfSlot;
+		dir = slotDir;
+		owner = ownedBy;
+		side = fireSlot;
+	}
+	void setContents(Equipment e) {
+		inslot = e;
+	}
+	void fire(ArrayList<FireingDirection> dirToFire) {
+		inslot.tick();
+		if (dirToFire.contains(side)) {
+			if (inslot.iswepon()) {
+				inslot.fire(dir, offset, owner);//how to handle this nicely?
+			}
+		}
+	}
+	void setEquip(Equipment e) {
+		inslot = e;
+	}
+	public float getProjSpeed() {
+		if (inslot.iswepon()) {
+			return inslot.getProjSpeed();
+		} else {
+			return 0f;
+		}
+	}
+	public float getProjLifetime() {
+		if (inslot.iswepon()) {
+			return inslot.getProjLifetime();
+		} else {
+			return 0f;
+		}
+	}
+}
